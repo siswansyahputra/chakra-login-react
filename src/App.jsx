32c4c86e0b024a911b104609f0fe70c407cb478c
Siswan Syahputra <siswansyahputra90@gmail.com>
2024-03-23 12:19:02 +0700

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,78 @@
+import React, { useState } from "react";
+import { ChakraProvider } from "@chakra-ui/react";
+import {
+  Button,
+  Checkbox,
+  Flex,
+  Text,
+  FormControl,
+  FormLabel,
+  Heading,
+  Input,
+  Stack,
+  Image,
+} from '@chakra-ui/react'
+
+const LoginForm = () => {
+  const [username, setUsername] = useState("");
+  const [password, setPassword] = useState("");
+
+  const handleSubmit = (e) => {
+    e.preventDefault();
+    // Lakukan sesuatu dengan data login
+    console.log("Username:", username);
+    console.log("Password:", password);
+  };
+
+  return (
+    <Stack minH={'100vh'} direction={{ base: 'column', md: 'row' }}>
+      <Flex p={8} flex={1} align={'center'} justify={'center'}>
+        <Stack spacing={4} w={'full'} maxW={'md'}>
+          <Heading fontSize={'2xl'}>Sistem Informasi Akademik</Heading>
+          <form onSubmit={handleSubmit}>
+            <FormControl id="Username">
+              <FormLabel>Username</FormLabel>
+              <Input type="text" value={username} onChange={(e) => setUsername(e.target.value)} />
+            </FormControl>
+            <FormControl id="password">
+              <FormLabel>Password</FormLabel>
+              <Input type="password" value={password} onChange={(e) => setPassword(e.target.value)} />
+            </FormControl>
+            <Stack spacing={6}>
+            <Stack
+              direction={{ base: 'column', sm: 'row' }}
+              align={'start'}
+              justify={'space-between'}>
+              <Checkbox>Remember me</Checkbox>
+              <Text color={'blue.500'}>Forgot password?</Text>
+            </Stack>
+            <Button type="submit" colorScheme={'blue'} variant={'solid'}>
+              Sign in
+            </Button>
+            </Stack>
+            </form>
+        </Stack>
+      </Flex>
+      <Flex flex={1}>
+        <Image
+          alt={'Login Image'}
+          objectFit={'cover'}
+          src={
+            'https://images.unsplash.com/photo-1486312338219-ce68d2c6f44d?ixid=MXwxMjA3fDB8MHxwaG90by1wYWdlfHx8fGVufDB8fHw%3D&ixlib=rb-1.2.1&auto=format&fit=crop&w=1352&q=80'
+          }
+        />
+      </Flex>
+    </Stack>
+
+  );
+};
+
+const App = () => {
+  return (
+    <ChakraProvider>
+      <LoginForm />
+    </ChakraProvider>
+  );
+};
+
+export default App;
